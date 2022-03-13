0ff33cd5b585d913065e431cd6c4b425a5fcf55b
norskemarcus <marc791j@stud.kea.dk>
2022-03-13 15:02:51 +0100

    I have added mine - marcus

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -1,8 +1,59 @@
 package com.company;
 
+import java.util.Scanner;
+/*
+Det er Adventure-klassen der skal bygge "kortet" ved at oprette og forbinde
+de ni room objekter - room objekterne må ikke være hardcoded med kortet på nogen måde,
+og der skal heller ikke oprettes arrays eller andre datastrukturer med “kortet”,
+kun forbindelser fra room-objekt til room-objekt
+ */
+
+
 public class Adventure {
 
+    enum Direction{
+        NORTH, EAST, SOUTH, WEST
+    }
+
+    public void playAdventure(){
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Direction dirN = Direction.NORTH;
+        Direction dirE = Direction.EAST;
+        Direction dirS = Direction.SOUTH;
+        Direction dirW = Direction.WEST;
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+        // starter spillet
+
+        //  oprette og forbinde de ni room objekter
+
+        // kun forbindelser fra room-objekt til room-objekt
+
+        // Man skal kunne skrive "go north", "go east", "go south" og "go west" for at navigere rundt.
+
+        // Hvis man kan flytte sig i den ønskede retning, udskrives navn og beskrivelse for det nye rum man er i - hvis det ikke er muligt at flytte sig,
+        // får man beskeden "you cannot go that way" og forbliver hvor man var.
+
+        // Al input/output og kommunikation med brugeren skal foregå i Adventure-objektet.
+
+        // Adventure-objektet holder hele tiden styr på hvilket rum spilleren befinder sig i
+        // - f.eks. med en variabel: currentRoom.
+
+        // exit - for at afbryde spillet helt, og afslutte programmet
+
+        // help - for at få en instruktion og oversigt over mulige kommandoer
+
+        // look - for at få gentaget beskrivelsen af det rum man er i
+
+
+
+
     }
 }
--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -1,4 +1,15 @@
 package com.company;
 
 public class Room {
+
+  // et rum med et navn, en beskrivelse og fire forbindelser til andre rum
+
+  // Room har 4 attributter af typen Room til at håndtere forbindelser til de fire andre rum:
+  // north, east, south and west eller null
+
+// Hvert room objekt har attributten name - det er en god ide at bruge samme værdi til denne attribut,
+// som navnet på variablen du opretter rummet med i Adventure-klassen.
+
+
+
 }
