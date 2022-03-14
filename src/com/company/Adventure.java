d75eb280d534a8efa2a20fc42e90b2c1f221ee15
tomm8231 <tomm8231@stud.kea.dk>
2022-03-14 16:39:21 +0100

    checkRoomNull functions added to all directions.

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -57,39 +57,38 @@ public class Adventure {
             System.out.println("To get instructions and a overview of possible commands, write: help  ");
             System.out.println("To exit the game, write: exit");
             input = sc.nextLine().toLowerCase();
+            //Programme does not run properly with .equals
+            // Does with .equalsIgnoreCase does. Why??
 
-            if (input.equalsIgnoreCase("look")) {
+            if (input.equals("look")) {
                 System.out.println("Looking around...");
                 System.out.println(currentRoom.getDescription());
 
-            } else if (input.equalsIgnoreCase("Go north")) {
+            } else if (input.equalsIgnoreCase("go north")) {
               //  System.out.println("Going north!");
                 Room caveToTheNorth = currentRoom.getNorth();
                 String direction = "north";
                 checkRoomNull(caveToTheNorth, direction);
-                // check for null
 
-            } else if (input.equalsIgnoreCase("Go south")) {
-                System.out.println("Going south!");
+            } else if (input.equalsIgnoreCase("go south")) {
                 Room caveToTheSouth = currentRoom.getSouth();
                 String direction = "south";
                 checkRoomNull(caveToTheSouth, direction);
 
             } else if (input.equalsIgnoreCase("Go east")) {
-                System.out.println("Going east!");
-
-                Room roomToTheEast = currentRoom.getEast();
-                currentRoom = roomToTheEast;
+                Room caveToTheEast = currentRoom.getEast();
+                String direction = "east";
+                checkRoomNull(caveToTheEast, direction);
 
             } else if (input.equalsIgnoreCase("Go west")) {
-                System.out.println("Going west!");
                 Room caveToTheWest = currentRoom.getWest();
-                currentRoom = caveToTheWest;
+                String direction = "west";
+                checkRoomNull(caveToTheWest, direction);
 
-            } else if (input.equalsIgnoreCase("help")) {
+            } else if (input.equals("help")) {
                 System.out.println("You use the commands 'go north', 'go east', 'go south' and 'go west' to change the " +
                     "direction in this game ");
-            } else if (input.equalsIgnoreCase("exit")) {
+            } else if (input.equals("exit")) {
                 System.out.println("You have ended the game - welcome back!");
             } else
                 System.out.println("Not a valid command, try again!");
@@ -111,15 +110,16 @@ public class Adventure {
 
     public void createCaves() {
         // N, S, E, W
+
         Room cave1 = new Room("the claustrophobia", "You are in a very small and dark cave with 4 tiny holes, you have no clues witch way to go. This cave is named the claustrophobia.");
-        Room cave2 = new Room("cave 2", "Here comes a description of cave 2");
-        Room cave3 = new Room(name, description);
-        Room cave4 = new Room("cave 4", "Here comes a description of cave 4");
-        Room cave5 = new Room(name, description);
-        Room cave6 = new Room(name, description);
-        Room cave7 = new Room(name, description);
-        Room cave8 = new Room(name, description);
-        Room cave9 = new Room(name, description);
+        Room cave2 = new Room("Cave 2", "Here comes a description of cave 2");
+        Room cave3 = new Room("Cave 3", "description3...");
+        Room cave4 = new Room("Cave 4", "Here comes a description of cave 4");
+        Room cave5 = new Room("Cave 5", "description5...");
+        Room cave6 = new Room("Cave 6", "description6");
+        Room cave7 = new Room("Cave 7", "description7");
+        Room cave8 = new Room("Cave 8", "description8");
+        Room cave9 = new Room("Cave 9", "description9");
 
         cave1.setEast(cave2);
         cave1.setSouth(cave4);
