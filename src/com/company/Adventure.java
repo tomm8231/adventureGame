cc65c36bf40762fa8171607f6606155386f96208
norskemarcus <marc791j@stud.kea.dk>
2022-03-16 15:10:59 +0100

    Refactored to Player and UserInterface

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -4,10 +4,8 @@ package com.company;
 public class Adventure {
 
 
-
     public static void main(String[] args) {
 
-        Adventure adventure = new Adventure();
         UserInterface ui = new UserInterface();
 
         ui.mainMenu();
--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -2,5 +2,47 @@ package com.company;
 
 public class Player {
 
+  private Room currentRoom;
 
+  public void setCurrentRoom(Room currentRoom) {
+    this.currentRoom = currentRoom;
+  }
+
+  public Room getCurrentRoom() {
+    return currentRoom;
+  }
+
+  public String movePlayer(String direction){
+    Room nextRoom = findNewRoom(direction);
+    return checkValidDirection(nextRoom, direction);
+
+  }
+
+  public String checkValidDirection(Room cave, String direction) {
+
+    if(cave == null) {
+      return "This way is ófærð. Try another path.";
+
+    } else {
+      currentRoom = cave;
+      return "Going " + direction + "...\n" +
+          "\n----------------\n" +
+          "You have entered \"" + currentRoom.getName() + "\":\n" +
+          currentRoom.getDescription() +
+          "\n----------------\n";
+    }
+  }
+
+  public Room findNewRoom(String direction){
+
+    if(direction.equals("north")) {
+      return currentRoom.getNorth();
+    } else if (direction.equals("south")) {
+      return currentRoom.getSouth();
+    } else if (direction.equals("east")) {
+      return currentRoom.getEast();
+    } else if (direction.equals("west")) {
+      return currentRoom.getWest();
+    } return null;
+  }
 }
